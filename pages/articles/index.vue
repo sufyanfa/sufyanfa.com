81b3c24ac16dc0ec21f58adfae9799bb6aab01ea
Sufyan Farea <sfmu1998@gmail.com>
2025-06-15 01:57:40 +0300

    docs: update article

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,11 +1,14 @@
 <template>
   <main class="min-h-screen">
     <AppHeader class="mb-16" title="المدونة" :description="description" />
-    <ul class="space-y-16">
+    <ul class="space-y-16" v-if="articles && articles.length">
       <li v-for="(article, id) in articles" :key="id">
         <AppArticleCard :article="article" />
       </li>
     </ul>
+    <p v-else class="text-center text-gray-500">
+      لا توجد مقالات متاحة في الوقت الحالي.
+    </p>
   </main>
 </template>
 
