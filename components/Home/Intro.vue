a50ac970556e79291379be31ec5c8e8c570695b2
Sufyan Farea <sfmu1998@gmail.com>
2026-03-31 14:36:02 +0300

    feat: add projects

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="space-y-6">
+  <div class="space-y-6 relative">
+  <div class="relative inline-block inline-flex items-center gap-4">
     <NuxtImg src="/avatar.jpg" alt="سفيان فارع"
       class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
       sizes="48px sm:64px" placeholder format="webp" />
+    <p
+    class="text-sm font-medium tracking-tight text-gray-800 dark:text-gray-100 bg-gray-200 dark:bg-gray-900 px-2 py-1 rounded">
+    سفيان فارع
+    </p>
+
+  </div>
     <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
-      مرحبًا، أنا سفيان فارع
+     شريكك التقني لتحويل فكرتك إلى منتج رقمي حقيقي
     </h1>
-    <h2 class="text-gray-900 dark:text-gray-400 text-lg">
-      شريكك التقني لبناء منتجات رقمية مبتكرة.
-    </h2>
-    <p class="text-gray-900 dark:text-gray-400">
-      أساعد رواد الأعمال والشركات في تحويل أفكارهم إلى منتجات رقمية ملموسة تحقق تأثيرًا حقيقيًا في السوق.
-من التخطيط والتصميم إلى التطوير والإطلاق، أرافقك خطوة بخطوة لضمان أن كل قرار تقني يُضيف قيمة حقيقية لمنتجك.
+    <p class="text-gray-900 dark:text-gray-400 text-lg">
+     أرافق رواد الأعمال من التخطيط وحتى التشغيل التجاري؛ لسد الفجوة بين التقنية ونموذج العمل، وضمان بناء منتج مستدام يُركز على تجربة المستخدم ويدفع عجلة النمو والربحية.
     </p>
   </div>
 </template>
